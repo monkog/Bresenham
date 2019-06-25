47e4421cd6c27f70787d303e14402843d42d0797
monkog <monkog@wp.pl>
2019-06-25 23:59:23 +0200

    Added ellipse tests

---
+++ SimplePaintTests/Shapes/CustomEllipseTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplePaint.Shapes;
+using System.Drawing;
+
+namespace SimplePaintTests.Shapes
+{
+	[TestClass]
+	public class CustomEllipseTests
+	{
+		[TestMethod]
+		public void Ctor_ValidParameters_PropertiesAssigned()
+		{
+			var position = new Point(1, 3);
+
+			var unitUnderTest = new CustomEllipse(position);
+
+			Assert.AreEqual(position, unitUnderTest.Position);
+		}
+	}
+}
