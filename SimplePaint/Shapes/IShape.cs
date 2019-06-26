60ceb141f70dfb583047a9120b42ffc150e018c4
monkog <monkog@wp.pl>
2019-06-26 21:25:18 +0200

    Added documentation to IShape

--- SimplePaint/Shapes/IShape.cs
+++ SimplePaint/Shapes/IShape.cs
@@ -7,6 +7,12 @@ namespace SimplePaint.Shapes
     /// </summary>
     public interface IShape
     {
+		/// <summary>
+		/// Draws the current shape.
+		/// </summary>
+		/// <param name="graphics">Graphics instance.</param>
+		/// <param name="color">Color of the shape.</param>
+		/// <param name="thickness">Thickness of the shape.</param>
         void Draw(Graphics graphics, Color color, int thickness);
     }
 }
