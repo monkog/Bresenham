8fdaccc9f59e026050a2cf1d3f46a8fd9d76364d
monkog <monkog@wp.pl>
2019-06-26 21:28:29 +0200

    Removed the redundant documentation from custom line

--- SimplePaint/Shapes/CustomLine.cs
+++ SimplePaint/Shapes/CustomLine.cs
@@ -8,58 +8,39 @@ namespace SimplePaint.Shapes
     /// </summary>
     public class CustomLine : IShape
     {
-        #region Public Properties
         /// <summary>
         /// Gets or sets the start point.
         /// </summary>
-        /// <value>
-        /// The start point.
-        /// </value>
         public Point StartPoint { get; set; }
+
         /// <summary>
         /// Gets or sets the end point.
         /// </summary>
-        /// <value>
-        /// The end point.
-        /// </value>
         public Point EndPoint { get; set; }
-        #endregion Public Properties
-        #region .ctor
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CustomLine"/> class.
-        /// </summary>
-        /// <param name="startPoint">The start point.</param>
-        /// <param name="endPoint">The end point.</param>
+
         public CustomLine(Point startPoint, Point endPoint)
         {
             StartPoint = startPoint;
             EndPoint = endPoint;
         }
-        #endregion .ctor
-        #region Public Methods
-        /// <summary>
-        /// Draws the line.
-        /// </summary>
-        /// <param name="graphics">The graphics.</param>
-        /// <param name="color">The color of the line.</param>
-        /// <param name="thickness">The thickness of the line.</param>
+
+        /// <inheritdoc/>
         public void Draw(Graphics graphics, Color color, int thickness)
         {
             SymmetricBresenham(graphics, color, thickness);
             Multisampling(graphics, color, thickness);
         }
-        #endregion Public Methods
-        #region Private Methods
+
         /// <summary>
-        /// Draws the line using the Symmetric Bresenham algorythm.
+        /// Draws the line using the Symmetric Bresenham algorithm.
         /// </summary>
         /// <param name="graphics">The graphics.</param>
         /// <param name="color">The color of the line.</param>
         /// <param name="thickness">The thickness of the line.</param>
         private void SymmetricBresenham(Graphics graphics, Color color, int thickness)
         {
-            int upPixels = (thickness - 1) / 2;
-            int downPixels = thickness - 1 - upPixels;
+            var upPixels = (thickness - 1) / 2;
+            var downPixels = thickness - 1 - upPixels;
 
             int x1 = StartPoint.X, x2 = EndPoint.X, y1 = StartPoint.Y, y2 = EndPoint.Y;
             int incrX, incrY, dx, dy;
@@ -74,6 +55,7 @@ namespace SimplePaint.Shapes
             else
                 DrawLine(graphics, color, y1, x1, y2, x2, dx, dy, incrY, incrX, upPixels, downPixels, isHorizontal: false);
         }
+
         /// <summary>
         /// Draws the line.
         /// </summary>
@@ -93,10 +75,10 @@ namespace SimplePaint.Shapes
         private static void DrawLine(Graphics graphics, Color color, int x1, int y1, int x2, int y2
             , int dy, int dx, int incrX, int incrY, int upPixels, int downPixels, bool isHorizontal)
         {
-            int xf = x1;
-            int yf = y1;
-            int xb = x2;
-            int yb = y2;
+            var xf = x1;
+            var yf = y1;
+            var xb = x2;
+            var yb = y2;
             var incrE = 2 * dy;
             var incrNe = 2 * (dy - dx);
             var d = 2 * dy - dx;
@@ -120,6 +102,7 @@ namespace SimplePaint.Shapes
                     DrawLineSegment(graphics, yf, xf, yb, xb, upPixels, downPixels, color, isHorizontal);
             }
         }
+
         /// <summary>
         /// Draws the line segment.
         /// </summary>
@@ -135,16 +118,16 @@ namespace SimplePaint.Shapes
         private static void DrawLineSegment(Graphics graphics, int x1, int y1, int x2, int y2
             , int upPixels, int downPixels, Color color, bool isLineHorizontal)
         {
-            int horizontalMultiplier = isLineHorizontal ? 1 : 0;
-            int verticalMultiplier = 1 - horizontalMultiplier;
-            for (int i = 0; i <= upPixels; i++)
+            var horizontalMultiplier = isLineHorizontal ? 1 : 0;
+            var verticalMultiplier = 1 - horizontalMultiplier;
+            for (var i = 0; i <= upPixels; i++)
             {
                 graphics.FillRectangle(new SolidBrush(color), new Rectangle(new Point(
                     x1 + i * horizontalMultiplier, y1 + i * verticalMultiplier), new Size(1, 1)));
                 graphics.FillRectangle(new SolidBrush(color), new Rectangle(new Point(
                     x2 + i * horizontalMultiplier, y2 + i * verticalMultiplier), new Size(1, 1)));
             }
-            for (int i = 0; i <= downPixels; i++)
+            for (var i = 0; i <= downPixels; i++)
             {
                 graphics.FillRectangle(new SolidBrush(color), new Rectangle(new Point(
                     x1 - i * horizontalMultiplier, y1 - i * verticalMultiplier), new Size(1, 1)));
@@ -152,6 +135,7 @@ namespace SimplePaint.Shapes
                     x2 - i * horizontalMultiplier, y2 - i * verticalMultiplier), new Size(1, 1)));
             }
         }
+
         /// <summary>
         /// Finds the drawing step.
         /// </summary>
@@ -194,13 +178,13 @@ namespace SimplePaint.Shapes
             bmp = new Bitmap(Math.Abs(StartPoint.X - EndPoint.X), Math.Abs(StartPoint.Y - EndPoint.Y), graphics);
             Point point;
             double alpha;
-            for (int i = 0; i < bmp.Width; i += 2)
-                for (int j = 0; j < bmp.Height; i += 2)
+            for (var i = 0; i < bmp.Width; i += 2)
+                for (var j = 0; j < bmp.Height; i += 2)
                 {
                     alpha = 0;
                     point = new Point(i / 2, j / 2);
-                    for (int k = 0; k < 2; k++)
-                        for (int l = 0; l < 2; l++)
+                    for (var k = 0; k < 2; k++)
+                        for (var l = 0; l < 2; l++)
                             if (bmp.GetPixel(k, l) == Color.Blue)
                                 alpha++;
                     alpha /= 4;
@@ -210,11 +194,10 @@ namespace SimplePaint.Shapes
 
         void IntensifyPixel(int x, int y, double distance, Graphics graphics, Color color)
         {
-            double intensity = Math.Round(Math.Abs(distance));
-            int intIntensity = (int)(intensity * 255);
+            var intensity = Math.Round(Math.Abs(distance));
+            var intIntensity = (int)(intensity * 255);
             graphics.FillRectangle(new SolidBrush(Color.FromArgb(intIntensity, color)), new Rectangle(new Point(x, y), new Size(1, 1)));
             //WritePixel(x, y, intensity);
         }
-        #endregion Private Methods
     }
 }
